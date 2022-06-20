c37cd9970f7eda9131a2114e86d747853632b830
Raushan <raushannair@gmail.com>
2022-06-20 06:12:51 +0530

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#alias
+alias backup='/usr/bin/git --git-dir=$HOME/Templates/Backup --work-tree=$HOME'
--- README.md
+++
@@ -1,2 +0,0 @@
-# Backup
-Backup for some local files
